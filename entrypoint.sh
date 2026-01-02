d1fafba7ca307f7209d5557ffa486efdc8177550
Calvin <603992850@qq.com>
2026-01-02 14:43:27 +0800

    feat:add real run logic

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,9 +16,14 @@ if [ $? -ne 0 ]; then
 fi
 
 LOG_FILE="/app/log/device_${SERIAL}.log"
-echo "Monkey starting on device: $SERIAL..."
-
 LOG_PATH="/app/log"
 mkdir -p "$LOG_PATH"
-echo "Running monkey test on $SERIAL at $(date)" > "$LOG_FILE" 2>&1
+
+echo "clear logcat: $SERIAL..."
+adb -s $SERIAL logcat -c
+echo "Monkey starting on device: $SERIAL..."
+adb -s $SERIAL shell monkey -p com.android.calculator2 -v 500 > "$LOG_FILE" 2>&1
+
+adb -s $SERIAL logcat -d >> "$LOG_FILE"
+
 echo "Test finished. Log saved to $LOG_FILE"
